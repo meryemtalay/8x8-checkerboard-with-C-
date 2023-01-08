cd77d9829fd33acb5c48ff2fa98b1d8a69c291f0
meryemtalay <meyremtlly@gmail.com>
2023-01-08 22:48:16 +0300

    C#'ta iç içe döngüyle dama tahtası

---
+++ RecapDemo/RecapDemo/Form1.cs
@@ -0,0 +1,70 @@
+/* 
+ Prepared by Meryem Talay
+ 8x8 checkerboard
+ */
+
+
+namespace RecapDemo
+{
+    public partial class Form1 : Form
+    {
+        //coonstructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+            Button[ , ] buttons = new Button[8,8];
+            int top = 0;
+            int left = 0;
+
+            for (int i = 0; i <= buttons.GetUpperBound(0); i++)
+            {
+               for (int j = 0; j <= buttons.GetUpperBound(1) ; j++)
+                {
+                    //nesne
+                    buttons[i,j] = new Button();
+                    buttons[i,j].Width = 50;
+                    buttons[i,j].Height = 50;
+                    buttons[i,j].Left = left;
+                    buttons[i,j].Top = top;
+                    left += 50;
+
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i,j].BackColor = Color.White;
+                    }
+                    else
+                    {
+                        buttons[i,j].BackColor = Color.Black;
+                    }
+                    
+                    this.Controls.Add(buttons[i,j]); 
+                   
+
+                }
+                top += 50;
+                left=0;
+
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    
+
